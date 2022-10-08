1db177cac2dbc8c628ff4ec416233dbc7a88c01c
Ali <anix1@kent.edu>
2022-10-07 18:09:53 -0600

    changes with gif added to readme

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// query selectors below:
+// query selectors below
 
 var clearButton = document.getElementById('clearButton');
 var messageButton = document.getElementById('receiveButton');
@@ -6,20 +6,18 @@ var meditateImage = document.getElementById('meditation');
 var messageText = document.getElementById('upliftingMessage');
 var radioButtons = document.getElementsByClassName("radio");
 
-// data below:
+// data below
 
 var affirmations = ['I forgive myself and set myself free.', 'I believe I can be all that I want to be.', 'I am in the process of becoming the best version of myself.', 'I have freedom & power to create the life I desire.', 'I choose to be kind to myself and love myself unconditionally.', 'My possibilites are endless.', 'I am worthy of my dreams.', 'I am enough.', 'I deserve to be healthy and feel good.', 'I am full of energy and vitality and my mind is calm and peaceful.', 'Every day I am getting healthier and stronger.', 'I am practicing mindfulness', 'I will detach myself from the outcome.'];
 
 var mantras = ['Breathing in, I send myself love. Breathing out, I send love to someone else who needs it.', 'Don\'t let yesterday take up too much of today.', 'Every day is a second chance.', 'Tell the truth and love everyone.', 'I am enough.', 'In the beginning it is you, in the middle it is you and in the end it is you.', 'Just do it.', 'I am present now.', 'Inhale the future, exhale the past.', 'This too shall pass.', 'Yesterday is not today.', 'The only constant is change.', 'Keep picking them up and putting them down.', 'I will not allow my emotions to control me.', 'I am not afraid to be wrong.'];
 
-// event listeners:
+// event listeners
 
 messageButton.addEventListener('click', selectMessage);
 clearButton.addEventListener('click', clearMessage);
 
-// initializers:
-// performed on every page load/refresh
-// to set initial page state
+// initializers
 
 for (var i = 0; i < radioButtons.length; i++) {
   radioButtons[i].addEventListener('click', enableMessageButton);
@@ -28,7 +26,7 @@ for (var i = 0; i < radioButtons.length; i++) {
 
 messageButton.disabled = true;
 
-// event handlers and functions:
+// event handlers and functions
 
 function selectMessage() {
   var messageType = "";
@@ -46,7 +44,6 @@ function selectMessage() {
   } else {
     return;
   }
-
   meditateImage.classList.add("hidden");
   clearButton.classList.remove("hidden");
   messageText.classList.remove("hidden");
